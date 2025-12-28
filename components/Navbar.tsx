c6b2635ab9ceb073308391415220e68096394453
shv-peri <harsha.vardhan@vation.com>
2025-12-28 13:29:18 +0530

    feat: implemeted posthog

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,36 @@
+'use client';
+
 import Link from "next/link";
 import Image from "next/image";
+import posthog from "posthog-js";
 
 const Navbar = () => {
+    const handleLogoClick = () => {
+        posthog.capture('logo_clicked', {
+            nav_location: 'header',
+        });
+    };
+
+    const handleNavClick = (linkName: string) => {
+        posthog.capture(`nav_${linkName.toLowerCase().replace(' ', '_')}_clicked`, {
+            nav_location: 'header',
+            link_name: linkName,
+        });
+    };
+
     return (
         <header>
             <nav>
-                <Link href='/' className="logo">
+                <Link href='/' className="logo" onClick={handleLogoClick}>
                     <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
 
                     <p>DevEvent</p>
                 </Link>
 
                 <ul>
-                    <Link href="/">Home</Link>
-                    <Link href="/">Events</Link>
-                    <Link href="/">Create Event</Link>
+                    <Link href="/" onClick={() => handleNavClick('Home')}>Home</Link>
+                    <Link href="/" onClick={() => handleNavClick('Events')}>Events</Link>
+                    <Link href="/" onClick={() => handleNavClick('Create Event')}>Create Event</Link>
                 </ul>
             </nav>
         </header>
